05324746b1f8d63d96e0055831164f9fc06a8fc6
micaelalay <micalay003@gmail.com>
2024-10-04 19:56:12 -0300

    changed dailyweather hook and getdate function

--- src/hooks/useDailyWeather.js
+++ src/hooks/useDailyWeather.js
@@ -11,7 +11,7 @@ export const useDailyWeather = () => {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `api.openweathermap.org/data/2.5/forecast`,
+        `https://api.openweathermap.org/data/2.5/forecast`,
         {
           params: {
             lat: city.lat,
@@ -21,13 +21,37 @@ export const useDailyWeather = () => {
           },
         }
       );
-      setDailyWeatherData(response.data);
+
+      const groupedData = groupByDay(response.data.list);
+      const dailyTemperatures = calculateDailyTemperatures(groupedData);
+
+      setDailyWeatherData(dailyTemperatures);
     } catch (error) {
       console.error("error fetching city data", error);
     } finally {
       setIsLoading(false);
     }
   }, []);
+
+  const groupByDay = (list) => {
+    return list.reduce((acc, item) => {
+      const date = item.dt_txt.split(" ")[0];
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(item);
+      return acc;
+    }, {});
+  };
+
+  const calculateDailyTemperatures = (groupedData) => {
+    return Object.keys(groupedData).map((date) => {
+      const temps = groupedData[date].map((item) => Math.round(item.main.temp));
+      const maxTemp = Math.max(...temps);
+      const minTemp = Math.min(...temps);
+      return { date, maxTemp, minTemp };
+    });
+  };
 
   return { dailyWeatherData, isLoading, getDailyWeather };
 };
--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,22 +1,35 @@
-export const getDate = (apiData) => {
-    const monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-  
-    const { year, month, day, dayOfWeek } = apiData;
-  
+export const getDate = (data) => {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  if (data.year && data.month && data.day && data.dayOfWeek) {
+    const { year, month, day, dayOfWeek } = data;
     const monthName = monthNames[month - 1];
-  
     return `${dayOfWeek} ${day}, ${monthName} ${year}`;
-  };
+  }
+
+  if (data.dt) {
+    const date = new Date(data.dt * 1000);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const dayOfWeek = dayNames[date.getDay()];
+    const monthName = monthNames[month];
+
+    return `${dayOfWeek} ${day}, ${monthName} ${year}`;
+  }
+};
