204aad1cd4a05ef955ea22d20ae3187d5e1290d1
micaelalay <micalay003@gmail.com>
2024-10-21 20:48:21 -0300

    error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "./Error.css";
 
 const Error = ({ error }) => {
-    return (
-        <div>{error}</div>
-    )
-}
+  return (
+    <div className="errorContainer">
+      <p className="errorText">{error}</p>
+      <button className="errorButton" onClick={() => window.location.reload()}>Reload page.</button>
+    </div>
+  );
+};
 
-export { Error }
+export { Error };
